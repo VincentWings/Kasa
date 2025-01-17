04bb00ded1f75e891062ba033a0fd99411cc6d03
Vincent Wings <wingsvincent@gmail.com>
2025-01-17 18:06:38 +0100

    add scss files

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
+import logo from "../assets/img/logo.svg"; // Adjust the path if necessary
 
 function Navbar() {
   return (
     <nav className="navbar">
-        <div>Logo</div>
+      <div className="container">
+        <div className="navbar__logo">
+          <img src={logo} alt="Kasa logo" />
+        </div>
+
+        <div className="navbar__nav">
+          <ul>
+            <li>
+              <a href="">Accueil</a>
+            </li>
+
+            <li>
+              <a href="">A propos</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
